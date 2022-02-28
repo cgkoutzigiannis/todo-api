af54a77448096c5b27c06c8b5094f0fb51bf0239
PCined <chrisles143@gmail.com>
2022-02-28 06:02:04 +0200

    POST /todos test

--- spec/requests/todo_spec.rb
+++ spec/requests/todo_spec.rb
@@ -20,5 +20,36 @@ describe "Todos API", type: :request do
     end 
   end
 
+  describe 'POST /todos' do
+    it 'creates new todo list' do
+        expect{
+            post '/todos', 
+            params: { 
+                todo: {
+                    title: 'My Test Todo List',
+                    description: 'Just a todo list for testing'
+                }
+            },
+            headers: {"Authorization" => "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.YvpeoTwNNC78GlPrVKCGbqvtjFDl_kTBcGjbY_gaQxA"}
+            
+        }.to change { Todo.count }.from(0).to(1)
+
+        expect(response).to have_http_status(:created)
+    end
+
+    it 'returns error if title is missing' do
+
+        post '/todos', 
+        params: { 
+            todo: {
+                description: 'Just a todo list for testing'
+            }
+        },
+        headers: {"Authorization" => "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.YvpeoTwNNC78GlPrVKCGbqvtjFDl_kTBcGjbY_gaQxA"}
+            
+        expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
 
 end
