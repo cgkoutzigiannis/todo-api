8c126ea730108e258ac2d76408ed1f36bde545ec
PCined <chrisles143@gmail.com>
2022-02-28 05:56:14 +0200

    get /todos test

--- spec/requests/todo_spec.rb
+++ spec/requests/todo_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "Todos", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+describe "Todos API", type: :request do
+  let!(:user) { FactoryBot.create(:user, username: 'chris', password: '12345') } 
+
+  describe 'GET /todos' do
+    let!(:todo) { FactoryBot.create(:todo, title: 'My first Todo', description: 'Testing first todo', user_id: user.id) }
+
+    it 'returns all todos' do
+        get '/todos', headers: {"Authorization" => "Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.YvpeoTwNNC78GlPrVKCGbqvtjFDl_kTBcGjbY_gaQxA"}
+
+        expect(response).to have_http_status(:success)
+        expect(JSON.parse(response.body).size).to eq(1)
+    end 
   end
+
+  
 end
